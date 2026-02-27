e6e1a0f21a805c81c17633764d7cf0a562c376c7
Andreas Würl <andi@tryb.de>
2026-02-27 08:57:01 +0100

    add sensor for internal topics

---
+++ src/sensor/DeviceSensor.h
@@ -0,0 +1,23 @@
+#ifndef DEVICE_SENSOR_H
+#define DEVICE_SENSOR_H
+
+#include "Sensor.h"
+
+namespace Sensor {
+
+    /**
+     * Virtual sensor that reports ESP32 device metrics:
+     * WiFi RSSI, chip temperature, free heap, uptime
+     */
+    class DeviceSensor : public Sensor {
+    public:
+        bool begin() override;
+        SensorReading read() override;
+        const char* getName() const override;
+        const char* getType() const override;
+        bool isConnected() override;
+    };
+
+} // namespace Sensor
+
+#endif // DEVICE_SENSOR_H
