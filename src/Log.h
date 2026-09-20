d0b220f29f61a1728a0122575baa0316cc651461
Andreas Würl <andi@tryb.de>
2026-09-20 11:29:28 +0200

    fix log indent

--- src/Log.h
+++ src/Log.h
@@ -30,7 +30,7 @@
 #undef ESP_LOGV
 
 #define _KLIMA_LOG(letter, tag, format, ...) do { \
-    Serial.printf("%08lu " #letter " %6s " format "\r\n", \
+    Serial.printf("%08lu " #letter " %8s " format "\r\n", \
                   (unsigned long)(esp_timer_get_time() / 1000ULL), \
                   tag, ##__VA_ARGS__); \
     if (SyslogOutput::isActive()) { \
